33c866b91dd4995e66833c38f60607fcfc04a335
Jay Christy <fortehlose@gmail.com>
2023-09-19 13:25:08 -0400

    ready

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 //! A simple 3D scene with light shining over a cube sitting on a plane.
 
+use std::string;
+
 use bevy::diagnostic::FrameTimeDiagnosticsPlugin;
 use bevy::diagnostic::LogDiagnosticsPlugin;
 use bevy::pbr::NotShadowCaster;
@@ -17,7 +19,6 @@ fn main() {
         .add_plugins(LogDiagnosticsPlugin::default())
         .add_plugins(FrameTimeDiagnosticsPlugin::default())
         .add_systems(Startup, setup)
-        .add_systems(Update, sping_wheels)
         .run();
 }
 
@@ -34,6 +35,14 @@ fn setup(
         ..default()
     });
 
+    // cube
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+        transform: Transform::from_xyz(-0.5, 0.5, 0.0),
+        ..default()
+    });
+
     // light
     commands.spawn(PointLightBundle {
         point_light: PointLight {
@@ -46,20 +55,19 @@ fn setup(
     });
     // camera
     commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        transform: Transform::from_xyz(0.0, 1.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
     });
-
+    let text: String = "Test Text".to_owned();
     //text?
-    let mesh_handle = meshes.add(create_mesh());
+    let mesh_handle = meshes.add(create_mesh(&text));
 
     commands.spawn((
         PbrBundle {
             mesh: mesh_handle,
             material: materials.add(StandardMaterial {
                 base_color: Color::BLUE,
-                emissive: Color::BLUE,
-                unlit: false,
+                unlit: true,
                 ..Default::default()
             }),
             transform: Transform::from_xyz(0.0, 0.5, 0.0),
@@ -70,15 +78,11 @@ fn setup(
     ));
 }
 
-fn sping_wheels() {
 
-    create_mesh();
-}
-
-fn create_mesh() -> Mesh {
+fn create_mesh(text: &String) -> Mesh {
     let mut cube_mesh = Mesh::new(PrimitiveTopology::TriangleList);
 
-    let mesh_data = get_text_vertices();
+    let mesh_data = get_text_vertices(&text);
     let chunks = mesh_data
         .vertices
         .chunks(3)
@@ -88,34 +92,20 @@ fn create_mesh() -> Mesh {
     #[rustfmt::skip]
     cube_mesh.insert_attribute(
         Mesh::ATTRIBUTE_POSITION,
-        // Each array is an [x, y, z] coordinate in local space.
-        // Meshes always rotate around their local [0, 0, 0] when a rotation is applied to their Transform.
-        // By centering our mesh around the origin, rotating the mesh preserves its center of mass.
         chunks,
     );
 
-    // Create the triangles out of the 24 vertices we created.
-    // To construct a square, we need 2 triangles, therefore 12 triangles in total.
-    // To construct a triangle, we need the indices of its 3 defined vertices, adding them one
-    // by one, in a counter-clockwise order (relative to the position of the viewer, the order
-    // should appear counter-clockwise from the front of the triangle, in this case from outside the cube).
-    // Read more about how to correctly build a mesh manually in the Bevy documentation of a Mesh,
-    // further examples and the implementation of the built-in shapes.
     #[rustfmt::skip]
     cube_mesh.set_indices(Some(Indices::U32(mesh_data.indices)));
-
     cube_mesh
 }
 
-fn get_text_vertices() -> IndexedMeshText {
+fn get_text_vertices(text: &String) -> IndexedMeshText {
     let font_data = include_bytes!("assets/FiraSans-Bold.ttf");
     let mut generator = MeshGenerator::new_without_cache(font_data, QualitySettings::default());
 
     let thing = generator
-        .generate_section("Test Text", true, None)
+        .generate_section(&text, true, None)
         .expect("Failed to generate glyph.");
-
-    generator.clear_cache();
-
     return thing;
 }
